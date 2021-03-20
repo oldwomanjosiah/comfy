007dfef278d0b90b30ce58b009a05188697759a7
Josiah Hilden <jhilden13@gmail.com>
2021-03-20 15:51:47 -0500

    Replaced hand rolled arg parsing with structopt
    - Makes non-positional arguments more robust
    - Has help messages built in so they will update as interface changes
    - includes version from cargo so will update as new versions published

--- src/base/parser.rs
+++ src/base/parser.rs
@@ -5,12 +5,12 @@ use std::{
     ffi::OsStr,
     fs::File,
     io::{stdin, BufRead, BufReader},
-    path::Path,
+    path::{Path, PathBuf},
     process::Command,
     thread, time,
 };
 
-pub fn parse(file: &str, show_comments: bool) {
+pub fn parse(file: &PathBuf, show_comments: bool) {
     let os = consts::OS;
     let pattern = ">";
 
@@ -83,11 +83,11 @@ fn kword(line: &str, index: usize) -> bool {
     }
 }
 
-fn check_file(file: &str) -> bool {
+fn check_file(file: &PathBuf) -> bool {
     if Path::new(file).is_file() && Path::new(file).extension() == Some(OsStr::new("comfy")) {
         true
     } else if Path::new(file).is_file() {
-        println!("{} is not a .comfy file, proceed? (y/N)", file);
+        println!("{} is not a .comfy file, proceed? (y/N)", file.display());
         let mut input = String::new();
 
         match stdin().read_line(&mut input) {
@@ -98,7 +98,7 @@ fn check_file(file: &str) -> bool {
             }
         }
     } else {
-        err_syntax(&format!("no such file named {}", &file));
+        err_syntax(&format!("no such file named {}", file.display()));
         false
     }
 }
